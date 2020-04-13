f557744069c07a98a0406c90a7bbbb727916b68b
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-13 19:15:08 +0700

    [globalvars.py] Change tuple to array

--- modules/globalvars.py
+++ modules/globalvars.py
@@ -2,31 +2,31 @@
 # Database stuff
 # kepemilikan_tiket: Array of pemilik_tiket (Username, ID_Wahana, Jumlah_Tiket)
 kepemilikan_tiket = []
-kepemilikan_default = ('','',0)
+kepemilikan_default = ['','',0]
 kepemilikan_count = 0
 # kritik_dan_saran: Array of kritik (Username, Tanggal_Kritik, ID_Wahana, Isi_Kritik)
 kritik_dan_saran = []
-kritik_default = ('','','','')
+kritik_default = ['','','','']
 kritik_count = 0
 # pembelian_tiket: Array of beli_tiket (Username, Tanggal_Pembelian, ID_Wahana, Jumlah_Tiket)
 pembelian_tiket = []
-pembelian_default = ('','','',0)
+pembelian_default = ['','','',0]
 pembelian_count = 0
 # penggunaan_tiket: Array of pengguna_tiket (Username, Tanggal_Penggunaan, ID_Wahana, Jumlah_Tiket)
 penggunaan_tiket = []
-penggunaan_default = ('','','',0)
+penggunaan_default = ['','','',0]
 penggunaan_count = 0
 # refund_tiket: Array of refund (Username, Tanggal_Refund, ID_Wahana, Jumlah_Tiket)
 refund_tiket = []
-refund_default = ('','','',0)
+refund_default = ['','','',0]
 refund_count = 0
 # users: Array of user (Nama, Tanggal_Lahir, Username, Password, Role, Saldo)
 users = []
-users_default = ('','','','','',0)
+users_default = ['','','','','',0]
 users_count = 0
 # list_wahana: Array of wahana (ID_Wahana, Nama_Wahana, Harga_Tiket, Batasan_Umur, Batasan_Tinggi)
 list_wahana = []
-list_wahana_default = ('','',0,0,False)
+list_wahana_default = ['','',0,0,False]
 list_wahana_count = 0
 
 # Global vars
