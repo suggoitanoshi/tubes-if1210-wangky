4cf532dfd3d8f87de7e3e0d6096e20f853dbeb68
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-22 06:59:49 +0700

    [main.py] Add login condition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import modules.globalvars as globalvars
+import modules.common as common
 
 #import modules.f01 as f01
 #import modules.f02 as f02
@@ -28,40 +29,43 @@ prompt = '$ '
 # Main Loop
 while not terminate:
   command = input(prompt)
-  if command == 'load':
-    pass # TODO: add f01 implementation
-  elif command == 'save':
-    pass # TODO: add f02 implementation
-  elif command == 'signup':
-    f03.sign_up()
-  elif command == 'login':
+  if command == 'login':
     f04.login()
-  elif command == 'cari_pemain':
-    f05.cari_pemain()
-  elif command == 'cari':
-    f06.search_wahana()
-  elif command == 'beli_tiket':
-    f07.buy_ticket()
-  elif command == 'main':
-    f08.use_ticket()
-  elif command == 'refund':
-    f09.refund()
-  elif command == 'kritik_saran':
-    f10.kritik_saran()
-  elif command == 'lihat_laporan':
-    f11.lihat_laporan()
-  elif command == 'tambah_wahana':
-    pass # TODO: add f12 implementation
-  elif command == 'topup':
-    f13.topup_saldo()
-  elif command == 'riwayat_wahana':
-    pass # TODO: add f14 implementation
-  elif command == 'tiket_pemain':
-    f15.tiket_pemain()
-  elif command == 'exit':
-    f16.exit()
-    terminate = True
-  elif command == 'best_wahana':
-    b03.best_wahana()
-  elif command == 'tiket_hilang':
-    b04.tiket_hilang()
+  elif common.has_login():
+    if command == 'load':
+      pass # TODO: add f01 implementation
+    elif command == 'save':
+      pass # TODO: add f02 implementation
+    elif command == 'signup':
+      f03.sign_up()
+    elif command == 'cari_pemain':
+      f05.cari_pemain()
+    elif command == 'cari':
+      f06.search_wahana()
+    elif command == 'beli_tiket':
+      f07.buy_ticket()
+    elif command == 'main':
+      f08.use_ticket()
+    elif command == 'refund':
+      f09.refund()
+    elif command == 'kritik_saran':
+      f10.kritik_saran()
+    elif command == 'lihat_laporan':
+      f11.lihat_laporan()
+    elif command == 'tambah_wahana':
+      pass # TODO: add f12 implementation
+    elif command == 'topup':
+      f13.topup_saldo()
+    elif command == 'riwayat_wahana':
+      pass # TODO: add f14 implementation
+    elif command == 'tiket_pemain':
+      f15.tiket_pemain()
+    elif command == 'exit':
+      f16.exit()
+      terminate = True
+    elif command == 'best_wahana':
+      b03.best_wahana()
+    elif command == 'tiket_hilang':
+      b04.tiket_hilang()
+  else:
+    print(globalvars.LOGIN_MESSAGE)
