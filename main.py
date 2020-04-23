7cc1f28965e500055666e772ae7c413aa619c701
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-23 12:23:17 +0700

    [main.py] Add debug stuff

--- main.py
+++ main.py
@@ -22,53 +22,61 @@ import modules.f16 as f16
 import modules.b03 as b03
 import modules.b04 as b04
 
-# Main Program variables
-terminate = False
-prompt = '$ '
+# DEBUGGING STUFF
+DEBUG = False
 
-# Main Loop
-while not terminate:
-  command = input(prompt)
-  if command == 'load':
-    f01.load()
-  elif globalvars.loaded:
-    if command == 'login':
-      f04.login()
-    elif common.has_login():
-      if command == 'signup':
-        f03.sign_up()
-      elif command == 'cari_pemain':
-        f05.cari_pemain()
-      elif command == 'cari':
-        f06.search_wahana()
-      elif command == 'beli_tiket':
-        f07.buy_ticket()
-      elif command == 'main':
-        f08.use_ticket()
-      elif command == 'refund':
-        f09.refund()
-      elif command == 'kritik_saran':
-        f10.kritik_saran()
-      elif command == 'lihat_laporan':
-        f11.lihat_laporan()
-      elif command == 'tambah_wahana':
-        f12.tambah_wahana()
-      elif command == 'topup':
-        f13.topup_saldo()
-      elif command == 'riwayat_wahana':
-        f14.riwayat_wahana()
-      elif command == 'tiket_pemain':
-        f15.tiket_pemain()
-      elif command == 'best_wahana':
-        b03.best_wahana()
-      elif command == 'tiket_hilang':
-        b04.tiket_hilang()
+# Main Program
+def main():
+  # Main Program variables
+  terminate = False
+  prompt = '$ '
+
+  # Main Loop
+  while not terminate:
+    command = input(prompt)
+    if command == 'load':
+      f01.load()
+    elif command == 'save':
+      f02.save()
+    elif command == 'exit':
+      f16.exit()
+      terminate = True
+    elif globalvars.loaded:
+      if command == 'login':
+        f04.login()
+      elif common.has_login():
+        if command == 'signup':
+          f03.sign_up()
+        elif command == 'cari_pemain':
+          f05.cari_pemain()
+        elif command == 'cari':
+          f06.search_wahana()
+        elif command == 'beli_tiket':
+          f07.buy_ticket()
+        elif command == 'main':
+          f08.use_ticket()
+        elif command == 'refund':
+          f09.refund()
+        elif command == 'kritik_saran':
+          f10.kritik_saran()
+        elif command == 'lihat_laporan':
+          f11.lihat_laporan()
+        elif command == 'tambah_wahana':
+          f12.tambah_wahana()
+        elif command == 'topup':
+          f13.topup_saldo()
+        elif command == 'riwayat_wahana':
+          f14.riwayat_wahana()
+        elif command == 'tiket_pemain':
+          f15.tiket_pemain()
+        elif command == 'best_wahana':
+          b03.best_wahana()
+        elif command == 'tiket_hilang':
+          b04.tiket_hilang()
+      else:
+        print(globalvars.LOGIN_MESSAGE)
     else:
-      print(globalvars.LOGIN_MESSAGE)
-  elif command == 'save':
-    f02.save()
-  elif command == 'exit':
-    f16.exit()
-    terminate = True
-  else:
-    print(globalvars.NOT_LOADED)
+      print(globalvars.NOT_LOADED)
+
+if not DEBUG:
+  main()
