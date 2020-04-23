28c254799bdef94c8b70a88f9001b7684229f68c
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-23 11:38:58 +0700

    [f16.py] Add save functionality

--- modules/f16.py
+++ modules/f16.py
@@ -1,8 +1,9 @@
 import modules.common as common
 import modules.globalvars as globalvars
+import modules.f02 as f02
 
 def exit():
     is_Exit=input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
     if is_Exit=="Y":
-        save()
+        f02.save()
     
