b01c9962ae309dc88d726be8ccfaa0c0e665b3ef
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-23 11:21:14 +0700

    [f01.py] use static method to access array

--- modules/f01.py
+++ modules/f01.py
@@ -11,148 +11,84 @@ def load():
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0],item[1],int(item[2]),item[3],item[4],item[5],int(item[6])]
+            jmlbrs += 1
         globalvars.users_count = jmlbrs
-        csv_file.seek(0)
-        #baca isi file, masukkan ke variabel csv_reader
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        #membuat array untuk array global
-        globalvars.users=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],int(a[2]),a[3],a[4],a[5],int(a[6])]
-            globalvars.users[i]=isi
-
+        
     #Daftar Wahana
     f_user=input('Masukkan nama File Daftar Wahana: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.list_wahana_count = jmlbrs
-        #baca isi file, masukkan ke variabel csv_reader
-        csv_file.seek(0)
-        #membuat array untuk array global
-        globalvars.list_wahana=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],int(a[2]),int(a[3]),bool(a[4])]
-            globalvars.list_wahana[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], int(item[2]), int(item[3]), bool(item[4])]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     #Pembelian Tiket
     f_user=input('Masukkan nama File Pembelian Tiket: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.pembelian_count = jmlbrs
-        csv_file.seek(0)
-        #baca isi file, masukkan ke variabel csv_reader        csv_reader = csv.reader(csv_file, delimiter=',')
-        #membuat array untuk array global
-        globalvars.pembelian_tiket=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],a[2],int(a[3])]
-            globalvars.pembelian_tiket[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], item[2], int(item[3])]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     #Penggunaan Tiket
     f_user=input('Masukkan nama File Penggunaan Tiket: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.penggunaan_count = jmlbrs
-        csv_file.seek(0)
-        #baca isi file, masukkan ke variabel csv_reader
-        #membuat array untuk array global
-        globalvars.penggunaan_tiket=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],a[2],int(a[3])]
-            globalvars.penggunaan_tiket[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], item[2], int(item[3])]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     #Kepemilikan Tiket
     f_user=input('Masukkan nama File Kepemilikan Tiket: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.kepemilikan_count = jmlbrs
-        #baca isi file, masukkan ke variabel csv_reader
-        csv_file.seek(0)
-        #membuat array untuk array global
-        globalvars.kepemilikan_tiket=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],int(a[2])]
-            globalvars.kepemilikan_tiket[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], int(item[2])]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     #Refund Tiket
     f_user=input('Masukkan nama File Refund Tiket: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.refund_count = jmlbrs
-        csv_file.seek(0)
-        #baca isi file, masukkan ke variabel csv_reader
-        #membuat array untuk array global
-        globalvars.refund_tiket=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],a[2],int(a[3])]
-            globalvars.refund_tiket[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], item[2], int(item[3])]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     #Kritik dan Saran
     f_user=input('Masukkan nama File Kritik dan Saran: ')    
     with open('external_files/'+f_user) as csv_file:
         #baca isi file, masukkan ke variabel csv_reader
         csv_reader = csv.reader(csv_file, delimiter=',')
-        #hitung baris yang ada dalam file masukkan ke variabel jmlbrs
+        #Masukkan data ke array global, catat jumlah data
         jmlbrs=0
-        for i in csv_reader:
-            jmlbrs=jmlbrs+1
-        #Replace semua isi Global Array dengan isi file
-        globalvars.kritik_count = jmlbrs
-        csv_file.seek(0)
-        #baca isi file, masukkan ke variabel csv_reader
-        #membuat array untuk array global
-        globalvars.kritik_dan_saran=[i for i in range (jmlbrs)]
-        #memasukkan data ke array global
-        for i in range (jmlbrs):
-            a=next(csv_reader)
-            isi=[a[0],a[1],a[2],a[3]]
-            globalvars.kritik_dan_saran[i]=isi
+        for item in csv_reader:
+            globalvars.users[jmlbrs] = [item[0], item[1], item[2], item[3]]
+            jmlbrs += 1
+        globalvars.users_count = jmlbrs
 
     globalvars.loaded = True
     print()
