18181426ee4f06b2f0031207b9fe8481a910ea0f
graciellavl <gracedmi.gvl@gmail.com>
2020-04-25 16:11:01 +0700

    [b02.py] edit dokumentasi [f03.py] edit fungsi

--- modules/f03.py
+++ modules/f03.py
@@ -19,7 +19,15 @@ def sign_up():
 
     hashed_pass = b01.hashpassword(input_pass)
 
-    globalvars.users[globalvars.users_count] = [input_nama,input_ttl,int(input_tinggi),input_user,hashed_pass,'Pemain',0,'default']
-    globalvars.users_count += 1
+    user_found = False
+    i = 0
+    while not user_found and  i < globalvars.users_count:
+        if input_user == globalvars.users[i][3]
+            user_found = True
+            print(input_nama, "telah terdaftar sebagai pemain, Silahkan Login dan Selamat Bermain.")
 
-    print("Selamat menjadi pemain, " + input_nama + ". Selamat bermain.")
+    if user_found == False:
+        globalvars.users[globalvars.users_count] = [input_nama,input_ttl,int(input_tinggi),input_user,hashed_pass,'Pemain',0,'default']
+        globalvars.users_count += 1
+
+        print("Selamat menjadi pemain, " + input_nama + ". Selamat bermain.")
