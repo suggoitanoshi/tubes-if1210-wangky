bf3be35edd1bf1b9b26f29c3ba3efc8c2434d9f0
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-04-25 19:39:16 +0700

    [f16.py] Switch f02 with f03

--- modules/f16.py
+++ modules/f16.py
@@ -1,9 +1,9 @@
 import modules.common as common
 import modules.globalvars as globalvars
-import modules.f02 as f02
+import modules.f03 as f03
 
 def exit():
     is_Exit=input("Apakah anda mau melakukan penyimpanan file yang sudah dilakukan (Y/N) ? ")
     if is_Exit=="Y" or is_Exit == 'y':
-        f02.save()
+        f03.save()
     
