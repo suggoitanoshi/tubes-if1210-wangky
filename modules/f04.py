60c1f2ab436752d2f75ccfafa76abe353c6cf9d6
graciellavl <gracedmi.gvl@gmail.com>
2020-04-26 17:51:41 +0700

    [f02.py] ralat fungsi [f04.py] ralat fungsi

--- modules/f04.py
+++ modules/f04.py
@@ -1,5 +1,6 @@
 import modules.globalvars as globalvars
 import modules.b01 as b01
+import modules.common as common
 
 # F04 - Sign Up User
 
@@ -10,25 +11,28 @@ def sign_up():
     Output : Penambahan data pada database
     ---
     """
-    input_nama = input("Masukkan nama pemain: ")
-    input_ttl = input("Masukkan tanggal lahir pemain (DD/MM/YYYY): ")
-    input_tinggi = input("Masukkan tinggi badan pemain (cm): ")
-    input_user = input("Masukkan username pemain: ")
-    input_pass = input("Masukkan password pemain: ")
-    print()
+    if common.is_admin():
+        input_nama = input("Masukkan nama pemain: ")
+        input_ttl = input("Masukkan tanggal lahir pemain (DD/MM/YYYY): ")
+        input_tinggi = input("Masukkan tinggi badan pemain (cm): ")
+        input_user = input("Masukkan username pemain: ")
+        input_pass = input("Masukkan password pemain: ")
+        print()
 
-    hashed_pass = b01.hashpassword(input_pass)
+        hashed_pass = b01.hashpassword(input_pass)
 
-    user_found = False
-    i = 0
-    while not user_found and  i < globalvars.users_count:
-        if input_user == globalvars.users[i][3]:
-            user_found = True
-            print(input_nama, "telah terdaftar sebagai pemain, Silahkan Login dan Selamat Bermain.")
-        else: i+=1
+        user_found = False
+        i = 0
+        while not user_found and  i < globalvars.users_count:
+            if input_user == globalvars.users[i][3]:
+                user_found = True
+                print(input_nama, "telah terdaftar sebagai pemain, Silahkan Login dan Selamat Bermain.")
+            else: i+=1
 
-    if user_found == False:
-        globalvars.users[globalvars.users_count] = [input_nama,input_ttl,int(input_tinggi),input_user,hashed_pass,'Pemain',0,'default']
-        globalvars.users_count += 1
+        if user_found == False:
+            globalvars.users[globalvars.users_count] = [input_nama,input_ttl,int(input_tinggi),input_user,hashed_pass,'Pemain',0,'default']
+            globalvars.users_count += 1
 
-        print("Selamat menjadi pemain, " + input_nama + ". Selamat bermain.")
+            print("Selamat menjadi pemain, " + input_nama + ". Selamat bermain.")
+    else:
+        print(globalvars.NOT_ADMIN_MESSAGE)
